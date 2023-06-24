7509e88fd8d300ddc52c0a9b734168cd9931c4d8
jun1485 <wnwjdwns1@naver.com>
2023-06-24 19:07:25 +0900

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>All in Job Test</p>
+        <p>All in Job Test2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
