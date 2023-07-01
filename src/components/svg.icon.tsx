8422dcbae48a1e8975a246019db1e1ad1ada2bad
nearworld <artemismars2@outlook.com>
2023-07-01 23:04:03 +0900

    chore: eslint airbnb, import order 설정

---
+++ src/components/svg.icon.tsx
@@ -0,0 +1,17 @@
+// import * as icons from "../assets/icons";
+
+// type IconName = keyof typeof icons;
+
+// type Props = {
+//   name: IconName;
+//   fill: string;
+// };
+
+// export const SvgIcon: React.FC<Props> = ({ name, fill }) => {
+//   const Icon = icons[name];
+//   return <Icon fill={fill} />;
+// };
+
+const random = () => undefined;
+
+export default random;
