736d66828e9837c3d65efd4a126260989094a66f
jun1485 <wnwjdwns1@naver.com>
2023-07-03 01:12:49 +0900

    design: header 퍼블리싱

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Header />
-        <p>React TypeScript Template</p>
       </header>
     </div>
   );
--- src/components/Navigation/Header/Header.tsx
+++ src/components/Navigation/Header/Header.tsx
@@ -1,7 +1,32 @@
+import './header.scss';
+
 function Header() {
   return (
-    <div className="header-container">
-      <div className="header-logo">로고</div>
+    <div>
+      <div className="header-container">
+        <div className="main-wrapper">
+          <div className="header-logo box">
+            <div>로고</div>
+          </div>
+          <div className="header-search-wrapper">
+            <input className="search-bar" type="text" placeholder="검색어를 입력하세요." />
+          </div>
+        </div>
+        <div className="header-character box">캐릭터</div>
+      </div>
+
+      <div className="menu-container">
+        <div className="menu-wrapper">
+          {/* 반복문으로 처리 예정 */}
+          <div className="menu-item">홈</div>
+          <div className="menu-item">공모전</div>
+          <div className="menu-item">대외활동</div>
+          <div className="menu-item">자격증</div>
+          <div className="menu-item">어학</div>
+          <div className="menu-item">인턴</div>
+          <div className="menu-item">취준job담</div>
+        </div>
+      </div>
     </div>
   );
 }
